2cc531c2dcd45c1e2163373e851d3ee87233fa73
Angie <shunshou@gmail.com>
2017-02-25 02:15:01 -0800

    saving annotation helpers but prob better to switch to yaml

--- tapeout/src/main/scala/transforms/AnnotationHelpers.scala
+++ tapeout/src/main/scala/transforms/AnnotationHelpers.scala
@@ -6,20 +6,20 @@ abstract class SimpleAnnotation {
   def serialize: String
 }
 
-trait AnnotationSerializer {
+abstract class AnnotationSerializer {
   def outerSplit: String = ","
   def innerSplit: String = ":"
   def serialize: String = getClass.getDeclaredFields.map { f => 
     f.setAccessible(true)
     val name = f.getName
     val value = f.get(this) match {
-      case b: SimpleAnnotation => a.serialize
+      case a: SimpleAnnotation => a.serialize
       case n: FirrtlNode => n.serialize
       case s: String => s
       case i: java.lang.Integer => s"$i"
       case d: java.lang.Double => s"$d"
-      case _ => throw new Exception("Invalid annotation value!")
+      case e => throw new Exception(s"Invalid annotation value! ${e.getClass}")
     }
-    Seq(annotationName, annotationValue).mkString(innerSplit)
+    Seq(name, value).mkString(innerSplit)
   }.mkString(outerSplit)
 }
