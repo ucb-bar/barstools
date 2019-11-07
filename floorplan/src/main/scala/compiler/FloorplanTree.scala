a6e13de3013b55de77a161ffb6b1af80a9099e38
John Wright <johnwright@eecs.berkeley.edu>
2019-11-07 10:55:50 -0800

    Start adding compiler infrastructure

---
+++ floorplan/src/main/scala/compiler/FloorplanTree.scala
@@ -0,0 +1,80 @@
+// See LICENSE for license details
+package barstools.floorplan.compiler
+
+import scala.collection.mutable.{HashMap}
+import barstools.floorplan.{Element, FloorplanState}
+
+class FloorplanTree(state: FloorplanState) {
+
+  class Node(val parent: Option[Node], val name: String) {
+    val children = new HashMap[String, Node]()
+    val elements = new HashMap[String, Element]()
+
+    def addElement(path: Seq[String], tag: String, element: Element): Unit = {
+      if (path.isEmpty) {
+        if (elements.contains(tag)) {
+          throw new Exception(s"Duplicate tag ${tag} at floorplan path ${getPath}")
+        } else {
+          elements.update(tag, element)
+        }
+      } else {
+        val child = children.getOrElseUpdate(path.head, new Node(Some(this), path.head))
+        child.addElement(path.tail, tag, element)
+      }
+    }
+
+    def getPathHelper(sub: String): String = parent.map(_.getPathHelper(s"${name}.${sub}")).getOrElse(sub)
+
+    def getPath: String = getPathHelper(name)
+
+    def getNode(path: Seq[String]): Node = {
+      if (path.isEmpty) {
+        this
+      } else {
+        children.get(path.head).map(_.getNode(path.tail)).getOrElse {
+          throw new Exception(s"Path ${getPath}.${path} does not exist in tree")
+        }
+      }
+    }
+
+    def getElement(tag: String): Element = {
+      elements.get(tag).getOrElse { throw new Exception(s"Tag ${tag} does not exist in node ${getPath}") }
+    }
+
+    def getElement(path: Seq[String], tag: String): Element = (if (path.isEmpty) this else getNode(path)).getElement(tag)
+  }
+
+  val root = new Node(None, "")
+
+  private def parsePathAndTag(str: String): (Seq[String], String) = {
+    val split = str.split("#", -1)
+    assert(split.size == 2, s"Malformed floorplan record path: ${str}")
+    val path = split(0).split(".")
+    val tag = split(1)
+    (path, tag)
+  }
+
+  state.elements.foreach { record =>
+    val (path, tag) = parsePathAndTag(record.path)
+    root.addElement(path, tag, record.element)
+  }
+
+  def getNode(path: Seq[String]): Node = root.getNode(path)
+
+  def getNode(str: String): Node = {
+    if (str.contains("#")) throw new Exception(s"getNode cannot be called on an element path (containing #): ${str}")
+    getNode(str.split("."))
+  }
+
+  def getElement(str: String): Element = {
+    val (path, tag) = parsePathAndTag(str)
+    root.getElement(path, tag)
+  }
+
+}
+
+object FloorplanTree {
+
+  def apply(state: FloorplanState) = new FloorplanTree(state)
+
+}
