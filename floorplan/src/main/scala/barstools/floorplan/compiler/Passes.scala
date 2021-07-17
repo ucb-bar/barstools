7bb7d2856cfa893fbf431be7c68f3a902b4de6fe
John Wright <johnwright@eecs.berkeley.edu>
2021-07-17 15:32:10 -0700

    Replace MemElementArray with MemMacroArray

--- floorplan/src/main/scala/barstools/floorplan/compiler/Passes.scala
+++ floorplan/src/main/scala/barstools/floorplan/compiler/Passes.scala
@@ -4,7 +4,9 @@ package barstools.floorplan.compiler
 import firrtl.annotations.TargetToken.{Instance, OfModule}
 
 import barstools.floorplan._
-import scala.collection.Map
+import scala.collection.{Map, Set}
+import scala.collection.mutable.{HashMap, HashSet}
+
 
 object FloorplanPasses {
 
@@ -43,13 +45,33 @@ class ResolveConstraintsPass extends Pass {
 }
 
 class TransformMemsPass(instMap: Map[OfModule, Seq[(Instance, OfModule)]]) extends Pass {
+
+  val nameSet = new HashSet[String]()
+  val renameMap = new HashMap[String, Set[String]]()
+
+  def getUniqueName(suggestion: String): String = {
+    // This is ugly and probably slow
+    var i = 0
+    var tempName = suggestion + s"_${i}"
+    while (nameSet.contains(tempName)) {
+      tempName = suggestion + s"_${i}"
+      i += 1
+    }
+    nameSet.add(tempName)
+    tempName
+  }
+
   def execute(state: FloorplanState): FloorplanState = {
-    val newRecords = state.records.map({ record =>
+    nameSet ++= state.records.map(_.element.name).toSet
+    // Need to update in two passes
+    val newRecords = state.records.flatMap({ record =>
       record.element match {
         case e: MemElement =>
           // TODO fail gracefully if key does not exist
           instMap(OfModule(record.memExt.get)).map { case (inst: Instance, ofMod: OfModule) =>
-            val element = AbstractMacro(ofMod.value)
+            nameSet.remove(e.name)
+            val element = AbstractMacro(getUniqueName(e.name + "_" + ofMod.value))
+            renameMap.update(e.name, renameMap.getOrElse(e.name, Set()) ++ Set(element.name))
             FloorplanElementRecord(
               root = record.root,
               inst = record.inst.map(_ + "/" + inst.value),
@@ -59,7 +81,26 @@ class TransformMemsPass(instMap: Map[OfModule, Seq[(Instance, OfModule)]]) exten
           }
         case _ => Seq(record)
       }
-    }).flatten
+    }).map({ record =>
+      record.element match {
+        case e: MemElementArray =>
+          val element = MemMacroArray(
+            name = e.name,
+            elements = e.elements.filter(_.isDefined).flatMap(x => renameMap(x.get)).map(x => Some(x)),
+            width = e.width,
+            height = e.height,
+            area = e.area,
+            aspectRatio = e.aspectRatio
+          )
+          FloorplanElementRecord(
+            root = record.root,
+            inst = record.inst, // should always be None
+            element = element,
+            memExt = None
+          )
+        case _ => record
+      }
+    })
     state.copy(records = newRecords)
   }
 }
